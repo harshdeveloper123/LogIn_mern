27ceabe1a3a94b5001ec2e785e260d723b4eaab3
harsh sharma <harshsunami75@gmail.com>
2025-07-28 19:58:50 +0530

    all done with frontend and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import Homepage from './pages/Homepage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import '../src/App.css'
 
-import './App.css'
+
+
 
 function App() {
-
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { CookiesProvider } from 'react-cookie'
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CookiesProvider>
+    <BrowserRouter>
     <App />
-  </StrictMode>,
+    </BrowserRouter>
+    </CookiesProvider>
+  </StrictMode>
 )
